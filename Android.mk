05d517a2937f354e58b892979a1a5afb50b41f7e
kallop <kallop2016@163.com>
2016-03-28 02:12:46 +0800

    v4a: fix build
    Change-Id: I6dc44949e9213ed685371b1c1a6b9f6cdd5e984f

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq (1,$(strip $(shell expr $(PLATFORM_SDK_VERSION) \>= 14)))
-
 LOCAL_PATH:= $(call my-dir)
 
 #ViPER4Android
@@ -12,7 +10,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 #LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 
-LOCAL_PACKAGE_NAME := ViPER4Android
+LOCAL_PACKAGE_NAME := v4a
 LOCAL_CERTIFICATE := platform
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
@@ -36,23 +34,3 @@ LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
     v4a_android-support:libs/android-support-v13.jar
 
 include $(BUILD_MULTI_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libv4a_fx_ics
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/soundfx
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_SRC_FILES := assets/libv4a_fx_ics.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := audio_effects
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/etc
-LOCAL_MODULE_SUFFIX := .conf
-LOCAL_SRC_FILES := system/etc/audio_effects.conf
-include $(BUILD_PREBUILT)
-
-endif
